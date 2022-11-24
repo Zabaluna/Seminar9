b59aad5f679767ce5908728853e4d486794eab0f
Наталья Ярцева <zabaluna@mail.ru>
2022-11-25 00:20:26 +0300

    1 to n rec

---
+++ Task63/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа
+//  в промежутке от 1 до N.
+
+Console.Clear();
+Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+
+PrintNumbers(1,n);
+
+void PrintNumbers(int start, int end)
+{
+    Console.Write($"{start} ");
+    if (start == end) return;
+    PrintNumbers(start + 1, end);
+}
---
+++ Task65/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+Console.Clear();
+Console.WriteLine("Введите M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+
+PrintNumbers(m,n);
+
+void PrintNumbers(int m, int n)
+{
+    Console.Write($"{m}, ");
+    if (m == n) return;
+    PrintNumbers(m + 1, n);
+}
