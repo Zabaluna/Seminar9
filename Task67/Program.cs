f5f3f9d38e81caf2add23881b34908bdb819a257
Наталья Ярцева <zabaluna@mail.ru>
2022-11-25 00:22:10 +0300

    Summa chisla

---
+++ Task67/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход 
+// число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+
+
+SumDigit(n);
+
+int SumDigit(int n)
+{
+    if (n == 0) return 0;
+    return n%10 + SumDigit(n/10);
+}
+Console.Write(SumDigit(n));
