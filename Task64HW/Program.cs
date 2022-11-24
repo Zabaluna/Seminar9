4247089f95f52d0d27be600c3102047e973ddc48
Наталья Ярцева <zabaluna@mail.ru>
2022-11-25 00:33:14 +0300

    subtraction M to N

---
+++ Task64HW/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу,
+//  которая выведет все натуральные числа в промежутке от M до N.
+// M = 5; N = 1. -> "5, 4, 3, 2, 1"
+Console.Clear();
+Console.WriteLine("Введите M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+
+PrintNumbers(m,n);
+
+void PrintNumbers(int m, int n)
+{
+    Console.Write($"{m}, ");
+    if (m == n) return;
+    PrintNumbers(m - 1, n);
+}
+
