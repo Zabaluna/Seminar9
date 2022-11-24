d8039d2d095b15d51a23f730f41486d8183f5ae3
Наталья Ярцева <zabaluna@mail.ru>
2022-11-25 00:26:50 +0300

    Pow A in to В

---
+++ Task69/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B,
+//  и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+Console.Clear();
+Console.Write("Введите число: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите степень: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{Pow(m,n)}");
+
+int Pow(int number,int rank)
+{
+    if (rank == 0) return 1;
+    if(rank==1) return number;
+    return (number * Pow(number,rank-1));
+}
